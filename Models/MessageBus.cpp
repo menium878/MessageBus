29ee80f2f6e56e56195b308ac51b2c3251e26d45
unknown <meus13@gmail.com>
2024-05-11 22:40:51 +0200

    MessageBus class start

---
+++ Models/MessageBus.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Menium on 5/11/2024.
+//
+
+#include "MessageBus.h"
---
+++ Models/MessageBus.h
@@ -0,0 +1,16 @@
+//
+// Created by Menium on 5/11/2024.
+//
+
+#ifndef MESSAGEBUS_MESSAGEBUS_H
+#define MESSAGEBUS_MESSAGEBUS_H
+
+
+class MessageBus {
+public:
+    MessageBus() = default;
+    ~MessageBus() = default;
+};
+
+
+#endif //MESSAGEBUS_MESSAGEBUS_H
